8115c4b3dabb39daba3a5e0e01881c0a56e2c0c7
Aquiles Diaz <aquilesdiaz335@gmail.com>
2023-11-16 02:03:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function respuesta(respuesta) {
+  if (respuesta === "si") {
+    alert("¡Eso es genial! ¡Gracias por decir que sí te amo mucho hermosa!");
+  } else {
+    alert("Oh, está bien. Gracias de todos modos.");
+  }
+}
