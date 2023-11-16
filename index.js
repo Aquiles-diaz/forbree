6be2aca6d71fe281cc4dbdcba09443f8a3267ac3
Aquiles Diaz <aquilesdiaz335@gmail.com>
2023-11-16 02:19:09 -0300

    ultimo commiteo

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function respuesta(respuesta) {
   if (respuesta === "si") {
-    alert("¡Eso es genial! ¡Gracias por decir que sí te amo mucho hermosa!");
+    alert(
+      "¡Eso es genial! ¡Gracias por decir que sí te amo mucho hermosa pero obvio despacio que fue una prueba para ver si estabas o no!"
+    );
   } else {
-    alert("Oh, está bien. Gracias de todos modos.");
+    alert(
+      "Oh, está bien. Gracias de todos modos y que vivas bien happy my hommie , la calle me respalda cada una de mis macanas."
+    );
   }
 }
